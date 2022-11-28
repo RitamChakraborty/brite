689cd0f6e875262a86182e9820fa932366724671
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2022-11-28 23:51:11 +0530

    Take brightness value input

--- brite.sh
+++ brite.sh
@@ -4,15 +4,23 @@ if [ $# -eq 0 ]
 then
   echo "Display current display and brightness"
 else
-  while getopts "gi" opt
-  do
+  ARG=$1
+  HYPHEN_INDEX=`expr "$ARG" : '-'`
+
+  if [ $HYPHEN_INDEX -eq 0 ]
+  then
+    echo "Change brightness to $ARG"
+  else
+    while getopts "id" opt
+    do
       case $opt in
         (i) # Increase brightness
           echo "Increase brightness" ;;
         (d) # Decrease brightness
           echo "Decrease brightness" ;;
-        (*) # Unknown option
+        (/?) # Unknown option
           printf "Illegal option '-%s'\n" "$opt" && exit 1 ;;
       esac
-  done
+    done
+  fi
 fi
