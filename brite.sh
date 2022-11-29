3b6aba631ff5a436fe2623639ed1b583a1a7a8d8
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2022-11-30 01:57:43 +0530

    Improve illegal option warning

--- brite.sh
+++ brite.sh
@@ -60,8 +60,9 @@ else
           echo "Increase brightness" ;;
         (d) # Decrease brightness
           echo "Decrease brightness" ;;
-        (/?) # Unknown option
-          printf "Illegal option '-%s'\n" "$opt" && exit 1 ;;
+        (*) # Unknown option
+          echo "Try 'brite --help' for more information."
+          exit 1
       esac
     done
   fi
